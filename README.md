b081e529b4ae38edbd29c74d6d752f4086701413
Enchan <enchant0130@gmail.com>
2021-02-26 14:54:57 +0900

    [Add] Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TweetOrganizer
+
+
+
+## Overview
+
+Automatically optimizes the Twitter timeline.
+
+## Licence
+
+This repository is published under MIT License.
+In details, see [LICENSE](LICENCE).
---
+++ main.py
@@ -0,0 +1,14 @@
+#
+# 伸びなかったツイートをアーカイブして自動削除
+#
+import dotenv, tweepy
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Ctrl+C")
+        exit(0)
